dc260755fea658431f3baa40a795b0feb424af2f
mihash <mikub97@gmail.com>
2021-03-23 13:03:33 +0100

    labels on graph draw, function for neighbours, correct diagonal initialisation

--- graph.py
+++ graph.py
@@ -7,7 +7,7 @@ import numpy as np
 class Graph(object):
 
     ## do poprawy
-    def __init__(self,n=10,p=0.7):
+    def __init__wrong(self,n=10,p=0.7):
         self.vertices_no = n
         self.vertices = list(range(n))
         self.graph = np.ones((n, n), dtype=int)
@@ -23,17 +23,61 @@ class Graph(object):
         print("Graph has ",np.sum(self.graph)-n, "edges (without the diagonal)")
         print(self.graph)
 
+    def __init__(self,n=10,p=0.7):
+        self.vertices_no=n
+        self.vertices = list(range(n))
+        self.graph = []
+        for i in range(n):
+            row = list(np.ones(n-i,dtype=int))
+            s = random.sample(list(range(1,n-i)),int((n-i)*p))
+            for j in s:
+                row[j]=0
+            for j in range(i):
+                row.insert(0,0)
+            self.graph.append(row)
+        print(np.matrix(self.graph))
+        for i in range(n):
+            for j in range(i):
+                self.graph[i][j] = self.graph[j][i]
+        print(np.matrix(self.graph))
 
     def show_graph(self):
-        rows, cols = np.where(self.graph == 1)
+        rows, cols = np.where(np.matrix(self.graph) == 1)
         edges = zip(rows.tolist(), cols.tolist())
         gr = nx.Graph()
-        all_rows = range(0, self.graph.shape[0])
+        all_rows = range(0, np.matrix(self.graph).shape[0])
         for n in all_rows:
             gr.add_node(n)
         gr.add_edges_from(edges)
-        nx.draw(gr, node_size=900)#, labels=mylabels, with_labels=True)
+        nx.draw(gr, node_size=900, with_labels=True)
         plt.show()
 
-g = Graph(n=20,p=0.9)
+    # function determines the neighbors of a given vertex
+    def N(self,vertex):
+        c = 0
+        l = []
+        for i in self.graph[vertex]:
+            if i == 1:
+                l.append(c)
+            c += 1
+        return l
+
+#the Bron-Kerbosch recursive algorithm
+# not working yet
+def bronk(g, r,p,x):
+    if len(p) == 0 and len(x) == 0:
+        print(r)
+        return
+    for vertex in p[:]:
+        r_new = r[::]
+        r_new.append(vertex)
+        p_new = [val for val in p if val in g.N(vertex)] # p intersects N(vertex)
+        x_new = [val for val in x if val in g.N(vertex)] # x intersects N(vertex)
+        bronk(g,r_new,p_new,x_new)
+        p.remove(vertex)
+        x.append(vertex)
+
+g = Graph(n=5,p=0)
+
+print("n(4)= ",g.N(4))
 g.show_graph()
